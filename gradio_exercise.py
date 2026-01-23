fcfdf4db8fc201dbbbb83b5c3b158aa5d1c85fda
Markus Brown <markus04b@gmail.com>
2026-01-23 16:10:05 +0000

    new gradio exercise for football

---
+++ gradio_exercise.py
@@ -0,0 +1,20 @@
+import sqlite3
+import gradio as gr
+import pandas as pd
+
+def fetch_points():
+    conn = sqlite3.connect('football.db')
+    cursor = conn.cursor()
+    query = """
+        SELECT *
+        FROM teams;
+    """
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    records_df = pd.DataFrame(results, columns=['id', 'x', 'y'])
+    return records_df
+
+iface = gr.Interface(fn=fetch_points, inputs=[], outputs=gr.Dataframe(headers=['id', 'x', 'y']))
+
+iface.launch()
